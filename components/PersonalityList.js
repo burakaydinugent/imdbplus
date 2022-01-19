7672ed2b5dbfeab4416b5a644143f292fc1b6fda
burakaydinugent <burak.aydin@ugent.bem>
2022-01-19 01:21:51 +0100

    artists tittle

--- components/PersonalityList.js
+++ components/PersonalityList.js
@@ -20,7 +20,7 @@ const PersonalityList = ({ data, level }) => {
   return (
 
     <div>
-      {items && items.length > 0 && <SmallCardList items={items} title="Don't miss the best artists" type="personality"></SmallCardList>}
+      {items && items.length > 0 && <SmallCardList items={items} title="The Artists you don't want to miss!" type="personality"></SmallCardList>}
     </div>
   );
 };
